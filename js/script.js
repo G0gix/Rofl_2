6b0298a7947406d4d55946bbcff3065e3f7828a4
G0gix <73593386+G0gix@users.noreply.github.com>
2021-01-12 18:35:14 +0500

    opa

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let name = prompt("Как тебя зовут?",);
+if(name == "Жора" || name == "жора" || name == "Георгий" || name == "георгий"){
+    alert("Давай не пизди");
+    location.href = location.href;
+}else{
+    let howMuchSuksDick = prompt("Сколько хуев сосал?",1);
+    alert(howMuchSuksDick);
+    if(howMuchSuksDick <= 0){
+        alert("Давай не пизди");
+    }else{
+        alert(`${name} ебать ты хуесос отсосал ${howMuchSuksDick} хуев`);
+    }
+}
+
+
+
